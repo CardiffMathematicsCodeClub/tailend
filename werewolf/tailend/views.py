3147d6a557d385e4438fca08ffc87cf1c5de6267
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-10-25 17:57:40 +0100

    Fixing structure.
    Divided the views, serializers and urls into different files.

--- werewolf/tailend/views.py
+++ werewolf/tailend/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer
 
-# Create your views here.
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
